7358fa6d74a0bfcdc7ee4b137b7fae3e194179c8
makjac <56642363+makjac@users.noreply.github.com>
2022-07-23 01:21:22 +0200

    auth repository

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -3,11 +3,54 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthRepository {
   final _firebaseAuth = FirebaseAuth.instance;
 
-  Future<void> signUp({required String email, required String passwd}) async {}
+  Future<void> signUp({required String email, required String passwd}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: passwd);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "email-already-in-use") {
+        throw Exception(
+            "There already exists an account with the given email address");
+      }
+      if (e.code == "weak-password") {
+        throw Exception("The password is not strong enough");
+      }
+      if (e.code == "invalid-email") {
+        throw Exception("The email address is not valid");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 
-  Future<void> signIn({required String email, required String passwd}) async {}
+  Future<void> signIn({required String email, required String passwd}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: passwd);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        throw Exception("There is no user corresponding to the given email");
+      }
+      if (e.code == "wrong-password") {
+        throw Exception("the password is invalid");
+      }
+      if (e.code == "user-disabled") {
+        throw Exception("the user has been disabled");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 
-  Future<void> signInUsingGoogle() async {}
+  Future<void> signInUsingGoogle() async {
+    //TODO
+  }
 
-  Future<void> signOut() async {}
+  Future<void> signOut() async {
+    try {
+      _firebaseAuth.signOut();
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 }
