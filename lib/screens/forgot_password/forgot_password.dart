b8066d8cd0c97f9fd740574c417fecae94e2bac1
makjac <56642363+makjac@users.noreply.github.com>
2022-07-23 02:20:27 +0200

    init forgot password page

---
+++ lib/screens/forgot_password/forgot_password.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Forgot Password"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
