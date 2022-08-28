5ec558577674f339a4ce77f77139d0ecf650378c
makjac <56642363+makjac@users.noreply.github.com>
2022-08-29 01:34:49 +0200

    StorageRepository update

--- lib/data/repository/fb_storage_repository.dart
+++ lib/data/repository/fb_storage_repository.dart
@@ -24,4 +24,40 @@ class StorageRepository {
       throw Exception(e.toString());
     }
   }
+
+  Future<void> deleteProfulrImgUrl(String? uid) async {
+    try {
+      var storageRef = _storage.ref().child("$profileImgLicatoion$uid");
+      await storageRef.delete();
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<String> getFileDownloadUrl(String? fileName, [String? path]) async {
+    try {
+      var storageRef = _storage.ref().child("$path$fileName");
+      return storageRef.getDownloadURL();
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> uploadFile(File file, String fileName, [String? path]) async {
+    try {
+      var storageRef = _storage.ref().child("$path$fileName");
+      await storageRef.putFile(file);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> deleteFile(String fileName, [String? path]) async {
+    try {
+      var storageRef = _storage.ref().child("$path$fileName");
+      await storageRef.delete();
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 }
